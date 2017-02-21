410b3e829c4b4108dbea8663077de26d99ff28b5
Administrator <Administrator>
2017-02-22 00:33:40 +0800

    ni大叔大婶

--- controllers/indexHomeController.js
+++ controllers/indexHomeController.js
@@ -4,12 +4,22 @@
 var app = angular.module('app');
 app.controller('indexHomeCtrl', function ($scope) {
     var vm = $scope.vm = {
+        icSearch:true,
         goActions:function(){
           alert("跳转")
         },
         actions:[{name:"生日提醒",url:"ic_birthday"},{name:"生日提醒",url:"ic_birthday"},{name:"习惯养成",url:"ic_habit"},{name:"学习计划",url:"ic_studyPlan"},{name:"习惯养成",url:"ic_habit"},{name:"习惯养成",url:"ic_habit"},{name:"个人流水",url:"ic_account"},{name:"生日提醒",url:"ic_birthday"}]
-    }
-        $scope.show=function(){
-            alert("dsdsds");
-        }
+    };
+    var search=document.getElementById('search');
+
+    search.onfocus=function(){
+        $scope.$apply(
+            vm.icSearch=false
+        )
+    };
+    search.onblur=function(){
+        $scope.$apply(
+            vm.icSearch=true
+        )
+    };
 });
