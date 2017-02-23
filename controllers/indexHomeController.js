7342d143093aa06fe3b8f199305b6098a31bf78a
2496212636@qq.com <2496212636@qq.com>
2017-02-23 12:50:59 +0800

    2017年2月22日16:15:58

--- controllers/indexHomeController.js
+++ controllers/indexHomeController.js
@@ -37,11 +37,11 @@ app.controller('indexHomeCtrl', function ($scope,$state,locals) {
             {name:"习惯养成",url:"ic_habit",num:"2"},
             {name:"习惯养成",url:"ic_habit",num:"2"},
             {name:"生日提醒",url:"ic_birthday",num:"1"}],
-        timer1:[{eventTime:"2小时21分钟",eventName:"信用卡还款",eventGrade:"1",code:"23231"},
-            {eventTime:"3小时1分钟",eventName:"生日",eventGrade:"2",code:"23232"},
-            {eventTime:"5小时2分钟",eventName:"参加例会",eventGrade:"3",code:"23243"},
-            {eventTime:"1天2小时09分钟",eventName:"交友",eventGrade:"4",code:"43324"},
-            {eventTime:"20天1小时32分钟",eventName:"房租",eventGrade:"2",code:"87633"}]
+        timer1:[{eventTime:"2小时21分钟",eventName:"信用卡还款323233232",eventGrade:"1",code:"23231"},
+            {eventTime:"3小时1分钟",eventName:"生日323233232",eventGrade:"2",code:"23232"},
+            {eventTime:"5小时2分钟",eventName:"参加例会323232323232",eventGrade:"3",code:"23243"},
+            {eventTime:"1天2小时09分钟",eventName:"交友23232323",eventGrade:"4",code:"43324"},
+            {eventTime:"1月20天12小时32分钟",eventName:"房租232323",eventGrade:"2",code:"87633"}]
     };
     for(var i=0;i<vm.timer1.length;i++){
         switch(vm.timer1[i].eventGrade){
