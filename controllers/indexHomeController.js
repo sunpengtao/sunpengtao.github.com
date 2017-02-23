d33b915149f931cf795e95905f0ad8407c15606a
2496212636@qq.com <2496212636@qq.com>
2017-02-23 14:51:58 +0800

    2017年2月22日16:15:58

--- controllers/indexHomeController.js
+++ controllers/indexHomeController.js
@@ -6,7 +6,21 @@ app.controller('indexHomeCtrl', function ($scope,$state,locals) {
     var vm = $scope.vm = {
         activeSlide:0,
         Search:false,
+        pDom:document.getElementsByClassName('controllerBox')[0].getElementsByTagName('p'),
         timer:[],
+        actions:[{name:"生日提醒",url:"ic_birthday",num:"1"},
+            {name:"习惯养成",url:"ic_habit",num:"2"},
+            {name:"个人流水",url:"ic_account",num:"3"},
+            {name:"学习计划",url:"ic_studyPlan",num:"4"},
+            {name:"生日提醒",url:"ic_birthday",num:"4"},
+            {name:"习惯养成",url:"ic_habit",num:"2"},
+            {name:"习惯养成",url:"ic_habit",num:"2"},
+            {name:"生日提醒",url:"ic_birthday",num:"1"}],
+        timer1:[{eventTime:"2小时21分钟",eventName:"信用卡还款323233232",eventGrade:"1",code:"23231"},
+            {eventTime:"3小时1分钟",eventName:"生日323233232",eventGrade:"2",code:"23232"},
+            {eventTime:"5小时2分钟",eventName:"参加例会323232323232",eventGrade:"3",code:"23243"},
+            {eventTime:"1天2小时09分钟",eventName:"交友23232323",eventGrade:"4",code:"43324"},
+            {eventTime:"1月20天12小时32分钟",eventName:"房租232323",eventGrade:"2",code:"87633"}],
         showSearch:function(){
             $state.go('search')
         },
@@ -29,19 +43,36 @@ app.controller('indexHomeCtrl', function ($scope,$state,locals) {
                     break;
             }
         },
-        actions:[{name:"生日提醒",url:"ic_birthday",num:"1"},
-            {name:"习惯养成",url:"ic_habit",num:"2"},
-            {name:"个人流水",url:"ic_account",num:"3"},
-            {name:"学习计划",url:"ic_studyPlan",num:"4"},
-            {name:"生日提醒",url:"ic_birthday",num:"4"},
-            {name:"习惯养成",url:"ic_habit",num:"2"},
-            {name:"习惯养成",url:"ic_habit",num:"2"},
-            {name:"生日提醒",url:"ic_birthday",num:"1"}],
-        timer1:[{eventTime:"2小时21分钟",eventName:"信用卡还款323233232",eventGrade:"1",code:"23231"},
-            {eventTime:"3小时1分钟",eventName:"生日323233232",eventGrade:"2",code:"23232"},
-            {eventTime:"5小时2分钟",eventName:"参加例会323232323232",eventGrade:"3",code:"23243"},
-            {eventTime:"1天2小时09分钟",eventName:"交友23232323",eventGrade:"4",code:"43324"},
-            {eventTime:"1月20天12小时32分钟",eventName:"房租232323",eventGrade:"2",code:"87633"}]
+        controllerBox:function(num){
+            for(var i=0;i<4;i++){
+                vm.pDom[i].style="font-size:16px;font-weight:normal";
+            }
+            vm.pDom[num].style="font-size:18px;font-weight:bold";
+            switch(num){
+                case 0:
+                    vm.styleBox='left:0';
+                    vm.activeSlide=0;
+                    break;
+                case 1:
+                    vm.styleBox='left:25%';
+                    vm.activeSlide=1;
+                    break;
+                case 2:
+                    vm.styleBox='left:50%';
+                    vm.activeSlide=2;
+                    break;
+                case 3:
+                    vm.styleBox='left:75%';
+                    vm.activeSlide=3;
+                    break;
+                case "b":
+                default:
+                    break;
+            }
+        },
+        slideHasChanged:function(index){
+            vm.controllerBox(index)
+        }
     };
     for(var i=0;i<vm.timer1.length;i++){
         switch(vm.timer1[i].eventGrade){
