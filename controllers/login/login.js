61400ffaf52430ff351232e122ba2866f8459534
SPT <2496212636@qq.com>
2017-03-17 21:22:45 +0800

    时间：2017年3月17日21:01:50； 内容：

--- controllers/login/login.js
+++ controllers/login/login.js
@@ -9,6 +9,8 @@ app.controller('loginsCtrl',function($scope,ui,alertCont){
       butStyles:["top:350px","top:420px","top:420px"],
       butConts:["登录","注册","设置"],
       activeSlides:[0,1,2],
+      begins:[0,2,6],
+      lasts:[1,5,9],
       inputCont:[[{src:"ic_userName",cont:"请输入登录账号",type:"text"},{src:"ic_passWorld",cont:"请输入登录密码",type:"password"}],
            [{src:"ic_userName",cont:"请输入登录账号",type:"text"},{src:"ic_passWorld",cont:"请输入登录密码",type:"password"},{src:"ic_surePassWorld",cont:"确认登录密码",type:"password"},{src:"ic_123Code",cont:"输入安全码(找回密码用)",type:"password"}],
            [{src:"ic_userName",cont:"请输入要找回的账号",type:"text"},{src:"ic_123Code",cont:"请输入安全码",type:"password"},{src:"ic_passWorld",cont:"请输入新的登录密码",type:"password"},{src:"ic_surePassWorld",cont:"确认新的登录密码",type:"password"}]
@@ -22,37 +24,27 @@ app.controller('loginsCtrl',function($scope,ui,alertCont){
           };
       },
       succeed:function(type){
-          vm.getDom();
-          if(type=="登录"){
-
-          }else {
-          }
-          for(var i=0;i<2;i++){
-              alertCont._null(vm.value[i])
-          }
+          console.log(vm.value);
+          for(var i=0;i<vm.value;i++){
+              //console.log(vm.value[i].val())
+          };
       },
       slideHasChanged:function(index){
           vm.butStyle=vm.butStyles[index];
           vm.butCont=vm.butConts[index];
           vm.activeSlide=vm.activeSlides[index];
-          var len,begin;
-          if(index==0){
-              len=2;
-              begin=0;
-          }else if(index==1){
-              begin=2;
-              len=4;
-          }else {
-              begin=2;
-              len=4;
-          }
-          vm.getDom()
+          var begin=vm.begins[index];
+          var last=vm.lasts[index];
+          vm.getDom(begin,last);
       },
-      getDom:function(){
+      getDom:function(begin,last){
           vm.value=[];
           var num=$(".val").size();
           for(var i=0;i<num;i++){
-              vm.value[i]=$(".val").eq(i).val("");
+              $(".val").eq(i).val("");
+          };
+          for(var i=begin,j=0;i<last,j<len;i++){
+              vm.value[i]=$(".val").eq(i);
           };
       }
   };
